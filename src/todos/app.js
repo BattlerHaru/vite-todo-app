51e6831af8554d16bf0bc1d31f305c59b83d8cf8
BattlerHaru <kronk.sdance@gmail.com>
2023-01-31 17:04:02 -0600

    funciones basicas en todoStore y helpers para crearlos en html

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,15 +1,42 @@
 // '?raw' = Vite lo importara en crudo
 import html from "./app.html?raw";
 
+import todoStore from "../store/todo.store";
+import {renderTodos} from "./helpers";
+
+const elementIds = {
+  TodoList: ".todo-list",
+  NewTodoInput: "#new-todo-input",
+};
+
 /**
  *
  * @param {String} elementId
  */
 export const App = (elementId) => {
+  const displayTodos = () => {
+    const todos = todoStore.getTodos(todoStore.getCurrentFilter());
+    renderTodos(elementIds.TodoList, todos);
+  };
+
   // Cuando la función App() se llama
   (() => {
     const app = document.createElement("div");
     app.innerHTML = html;
     document.querySelector(elementId).append(app);
+    displayTodos();
   })();
+
+  // Referencias HTML
+  const newDescriptionInput = document.querySelector(elementIds.NewTodoInput);
+
+  // Listeners
+  newDescriptionInput.addEventListener("keyup", (event) => {
+    if (event.keyCode !== 13) return;
+    if (event.target.value.trim().length === 0) return;
+
+    todoStore.addTodo(event.target.value);
+    displayTodos();
+    event.target.value = "";
+  });
 };
